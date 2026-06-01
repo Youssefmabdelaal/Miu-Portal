fd1f9e84dc5af5d14cfd65ca7214436ecd6996f9
Youssef <youssef2405244@miuegypt.edu.eg>
2026-06-01 10:00:00 +0300

    Implement part 1 of Authentication & Core Setup

---
+++ smart-university-system/controllers/Authentication___Core_Setup_controller.js
@@ -0,0 +1,8 @@
+// Commit 1 by Youssef for Authentication & Core Setup
+
+// Commit 6 by Youssef for Authentication & Core Setup
+
+// Commit 11 by Youssef for Authentication & Core Setup
+
+// Commit 16 by Youssef for Authentication & Core Setup
+
